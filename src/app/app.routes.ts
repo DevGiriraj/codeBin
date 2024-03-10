a50239fed04d837374cdc9a2f8a63d86f654a05e
DevGiriraj <feb_dev@outlook.com>
2024-03-10 13:01:25 +0530

    routes and lazy loading component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './component/login/login.component';
 import { SignupComponent } from './component/signup/signup.component';
+import { NotFoundComponent } from './component/not-found/not-found.component';
+import { AboutComponent } from './component/about/about.component';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
     { path: 'signup', component: SignupComponent },
+    { path: 'about', loadComponent: () => import('./component/about/about.component').then(m => m.AboutComponent)},
+    {path:'',redirectTo:"/login",pathMatch:"full"},
+    {path:'**', component:NotFoundComponent}
 
 ];
